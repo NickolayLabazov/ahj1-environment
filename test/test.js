1a698cce7ae11acf2f6bc50a367dac8ba50dd4ff
Николай Лабазов <Nickolaylabazov@yandex.ru>
2019-03-30 17:27:39 +0300

    30.03

--- test/test.js
+++ test/test.js
@@ -1,6 +1,4 @@
-//import Character, { classes } from '../src/js/Character.js';
-
- test('Тест', () => {
+test('Тест', () => {
   const expected = 1;
   const received = 1;
   expect(received).toBe(expected);
