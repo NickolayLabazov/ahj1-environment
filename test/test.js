6be3f1c86bfe9b417d3a9b8c4f5c578fade22a62
Николай Лабазов <Nickolaylabazov@yandex.ru>
2019-03-30 16:41:17 +0300

    30.03

---
+++ test/test.js
@@ -0,0 +1,8 @@
+//import Character, { classes } from '../src/js/Character.js';
+
+ test('Тест', () => {
+  const expected = 1;
+  const received = 1;
+  expect(received).toBe(expected);
+});
+ 
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: 'development',
+  entry: {
+    app: './js/app.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: '../ahj1-environment',
+  },
+
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
