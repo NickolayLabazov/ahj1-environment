82b44a5a5d8cfddf0f078f59df8ae479a0c46c5a
Николай Лабазов <Nickolaylabazov@yandex.ru>
2019-04-18 12:22:22 +0300

    30.03

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import './js/app.js';
 
 const fav = document.createElement('link');
 fav.setAttribute('rel', 'shortcut icon');
-fav.setAttribute('href', favicon);
+fav.setAttribute('href', `.${favicon}`);
 fav.setAttribute('type', 'image/x-icon');
 document.head.appendChild(fav);
 
 const img = document.createElement('img');
-img.setAttribute('src', js);
+img.setAttribute('src', `.${js}`);
 const h = document.querySelector('h1');
 document.body.insertBefore(img, h);
